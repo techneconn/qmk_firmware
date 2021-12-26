1f36f58a57d5ed4c83bafca606db6ff9fd36923c
yuukive <13178976+yuukive@users.noreply.github.com>
2021-12-26 17:48:25 +0900

    format rules

--- keyboards/crkbd/keymaps/yuukive/config.h
+++ keyboards/crkbd/keymaps/yuukive/config.h
@@ -32,7 +32,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUP
 #define FORCE_NKRO
 
 #ifdef RGBLIGHT_ENABLE
